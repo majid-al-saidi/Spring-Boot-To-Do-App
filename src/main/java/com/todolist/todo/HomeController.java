5894c1c050c3f5c6d1bd8b6aa973e8325b24685d
majid-al-saidi <majid-al-saidi@outlook.com>
2025-05-14 12:01:58 +0400

    Task Model + list tasks in html

--- src/main/java/com/todolist/todo/HomeController.java
+++ src/main/java/com/todolist/todo/HomeController.java
@@ -1,14 +1,24 @@
 package com.todolist.todo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index"; // Thymeleaf will load templates/index.html
+    public String home(Model model) {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1L , "Learning Java", false));
+        tasks.add(new Task(2L, "Buy milk", false));
+        tasks.add(new Task(3L, "Finish homework", true));
+        tasks.add(new Task(4L, "Read a book", false));
+
+        model.addAttribute("tasks" , tasks);
+        return "index";
     }
 }
